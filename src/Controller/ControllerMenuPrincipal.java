04bdbd44ecbafc110a24b88aa4549f50d5e7031c
joaolbasso <121301613+joaolbasso@users.noreply.github.com>
2024-07-16 11:53:40 -0300

    Iniciando Menu Principal (FX)

--- src/Controller/ControllerMenuPrincipal.java
+++ src/Controller/ControllerMenuPrincipal.java
@@ -6,6 +6,8 @@ package Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 /**
@@ -15,8 +17,30 @@ import javafx.fxml.Initializable;
  */
 public class ControllerMenuPrincipal implements Initializable {
 
+    @FXML
+    protected void entrarCaixa(ActionEvent event) {
+        System.out.println("Clique Caixa");
+    }
+    
+    @FXML
+    protected void entrarDespesas(ActionEvent event) {
+        System.out.println("Clique Despesas");
+    }
+    
+    @FXML
+    protected void entrarClientes(ActionEvent event) {
+        System.out.println("Clique Clientes");
+    }
+    
+    @FXML
+    protected void entrarItens(ActionEvent event) {
+        System.out.println("Clique Itens");
+    }
+    
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
