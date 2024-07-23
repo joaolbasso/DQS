63a01d187c33c372deb1ac8e85981662fba3fc61
joaolbasso <121301613+joaolbasso@users.noreply.github.com>
2024-07-23 08:58:58 -0300

    Cadastrar Beneficiario arrumado bugs de métodos

--- src/ControllerCadastrarBeneficiario.java
+++ src/ControllerCadastrarBeneficiario.java
@@ -3,9 +3,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -14,6 +23,9 @@ import javafx.fxml.Initializable;
  */
 public class ControllerCadastrarBeneficiario implements Initializable {
 
+    @FXML
+    private TextField txtfldNomeBeneficiario;
+    
     /**
      * Initializes the controller class.
      */
@@ -21,5 +33,21 @@ public class ControllerCadastrarBeneficiario implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    public void registrarDespesa(ActionEvent event) {
+        System.out.println(txtfldNomeBeneficiario.getText());
+    }
+    
+    public void limparCampos(ActionEvent event) {
+        txtfldNomeBeneficiario.setText("");
+    }
+    public void voltarParaDespesa(ActionEvent event) throws IOException {
+        Parent registrarDespesaView = FXMLLoader.load(getClass().getResource("/View/RegistrarDespesa.fxml"));
+        Scene registrarDespesaScene = new Scene(registrarDespesaView);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(registrarDespesaScene);
+        window.show();
+    }
+    
     
 }
