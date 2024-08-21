df6d84be9c2da87d28c38b2127c52708128fff1f
joaolbasso <121301613+joaolbasso@users.noreply.github.com>
2024-08-21 11:04:29 -0300

    feat: Trazendo dados do banco para TableView de Itens

--- src/ControllerItem.java
+++ src/ControllerItem.java
@@ -1,6 +1,10 @@
+import DAO.ItemDAO;
+import Model.Item;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,7 +13,12 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import org.hibernate.PropertyValueException;
 
 /**
  * FXML Controller class
@@ -22,6 +31,24 @@ public class ControllerItem implements Initializable {
     @FXML
     private Button btnRegistrarItem;
     
+    @FXML
+    private TableView<Item> tbvwItens;
+    
+    @FXML
+    private TableColumn<Item, Integer> id_item = new TableColumn<>("Id");
+    
+    @FXML
+    private TableColumn<Item, String> nome_item = new TableColumn<>("Nome");
+    
+    @FXML
+    private TableColumn<Item, Double> valor_item = new TableColumn<>("Valor");
+    
+    @FXML
+    private TableColumn<Item, Integer> quantidade = new TableColumn<>("Quantidade");
+    
+    @FXML
+    private TableColumn<Item, Character> tipo_item = new TableColumn<>("Tipo");
+    
     public void voltarMenuPrincipal(ActionEvent event) throws IOException {
         
         Parent caixaView = FXMLLoader.load(getClass().getResource("/View/MenuPrincipal.fxml"));
@@ -32,6 +59,9 @@ public class ControllerItem implements Initializable {
         window.show();
     }
     
+    ItemDAO itemDAO = new ItemDAO();
+    ObservableList<Item> itens = FXCollections.observableArrayList(itemDAO.todosOsItens());
+    
     public void entrarCadastrarItem(ActionEvent event) throws IOException {
         Parent cadastrarItemView = FXMLLoader.load(getClass().getResource("/View/CadastrarItem.fxml"));
         Scene cadastrarItemScene = new Scene(cadastrarItemView);
@@ -40,12 +70,38 @@ public class ControllerItem implements Initializable {
         window.show();
     }
     
+        private <T> void centralizarTextoNaColuna(TableColumn<T, String> coluna) {
+    coluna.setCellFactory(column -> {
+        return new TableCell<T, String>() {
+            @Override
+            protected void updateItem(String item, boolean empty) {
+                super.updateItem(item, empty);
+                if (item == null || empty) {
+                    setText(null);
+                    setStyle("");
+                } else {
+                    setText(item);
+                    setStyle("-fx-alignment: CENTER;");
+                }
+            }
+        };
+    });
+}
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        id_item.setCellValueFactory(new PropertyValueFactory<>("id_item"));
+        nome_item.setCellValueFactory(new PropertyValueFactory<>("nome_item"));
+        valor_item.setCellValueFactory(new PropertyValueFactory<>("valor_item"));
+        quantidade.setCellValueFactory(new PropertyValueFactory<>("quantidade"));
+        tipo_item.setCellValueFactory(new PropertyValueFactory<>("tipo_item"));
+        
+        centralizarTextoNaColuna(nome_item);
+        
+        tbvwItens.setItems(itens);
     }    
     
 }
--- src/DAO/ItemDAO.java
+++ src/DAO/ItemDAO.java
@@ -1,7 +1,11 @@
 package DAO;
 
+import Model.Cliente;
 import Model.Item;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 public class ItemDAO extends AbstractDAO {
     
@@ -24,6 +28,37 @@ public class ItemDAO extends AbstractDAO {
         }
     }
 
+    public List<Item> todosOsItens() {
+        List<Item> todosOsItens = new ArrayList<>();
+        
+        try {
+            em = EntityManagerFactorySingleton.getInstance().createEntityManager();
+            
+            em.getTransaction().begin();
+            
+                String jpql = "SELECT i FROM Item i";
+                TypedQuery<Item> query = em.createQuery(jpql, Item.class);
+                todosOsItens = query.getResultList();
+                
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            if (em != null) {
+            em.getTransaction().rollback();
+            }
+             e.printStackTrace();
+        } finally {
+            if (em != null && em.isOpen()) {
+                em.close();
+            }
+        }
+        
+        if (todosOsItens != null) {
+            return todosOsItens;
+        }
+        List<Item> itensVazio = new ArrayList<>();
+        return itensVazio;
+    }
+    
     public void update() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
