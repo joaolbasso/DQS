4ed2d9f2ca0554114ddee4895fbfe3a8af64ffa6
joaolbasso <121301613+joaolbasso@users.noreply.github.com>
2024-10-11 11:13:50 -0300

    Relatorios em mal funcionamento

--- src/ControllerRelatorio.java
+++ src/ControllerRelatorio.java
@@ -1,5 +1,10 @@
+import DAO.EntityManagerFactorySingleton;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.sql.Connection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -9,14 +14,58 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.view.JasperViewer;
 
 public class ControllerRelatorio implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        System.out.println("ALO");
     }
+    
+    @FXML
+    public void relatorioTeste(ActionEvent event) throws IOException, JRException {
+        gerarRelatorio("a", "b");
+    }
+    
 
+    public void gerarRelatorio(String filtro1, String filtro2) {
+        EntityManagerFactory emf = EntityManagerFactorySingleton.getInstance();
+        EntityManager em = null;
+
+        try {
+            em = emf.createEntityManager();
+            Connection connection = DAO.DBConnection.getConnection();
+
+            // Construir a query dinâmica
+            String query = "SELECT * FROM cliente";
+
+            // Use o caminho relativo para o arquivo .jrxml
+            InputStream inputStream = getClass().getClassLoader().getResourceAsStream("Relatorio/query.jrxml");
+            if (inputStream == null) {
+                throw new IllegalArgumentException("Arquivo de relatório não encontrado: Relatorio/query.jrxml");
+            }
+
+            JasperReport jasperReport = JasperCompileManager.compileReport(inputStream);
+            Map<String, Object> parameters = new HashMap<>();
+            parameters.put("QUERY", query);
+
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, connection);
+            JasperViewer.viewReport(jasperPrint, false);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+    
     @FXML
     public void voltar(ActionEvent event) throws IOException {
         String nomeDaView = "MenuPrincipal.fxml";
